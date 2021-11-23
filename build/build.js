42cb24ddac9d41a69f8d167686433512eeb4cb3d
Danny Cairns <dpcairns@gmail.com>
2021-11-22 16:23:39 -0800

    add bug

--- build/build.js
+++ build/build.js
@@ -95,8 +95,6 @@ function displayStats() {
 }
 
 function displayCatchphrases() {
-    chatchphrasesEl.textContent = '';
-
     for (let catchphrase of catchphrases) {
         const p = document.createElement('p');
 
